6d318cb0f9545f6dcce7a8116803b0859c888b6c
Kristie Chow <kristiemhchow@gmail.com>
2016-02-11 10:03:38 -0500

    Update names

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Discly
+module discvr
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
